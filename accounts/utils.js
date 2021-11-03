cfe22613602d1556d9732d3d7646d556873e8c07
adityayaduvanshi0001 <adityayaduvanshi0001@gmail.com>
2021-11-03 15:26:01 +0530

    removed test account

--- accounts/utils.js
+++ accounts/utils.js
@@ -20,16 +20,6 @@ const isPhone = async (phone) => {
 };
 
 const sendOTPByMail = async (to, otp) => {
-  /*const testAccount = await nodemailer.createTestAccount(),
-    transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
-      auth: {
-        user: testAccount.user,
-        pass: testAccount.pass,
-      },
-    });*/
   const transporter = nodemailer.createTransport({
       service: "hotmail",
       host: "smtp-mail.outlook.com",
